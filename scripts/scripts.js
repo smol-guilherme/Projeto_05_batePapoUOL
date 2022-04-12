8463d5c571e6877c39dd576fee1d637db01278c9
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2022-04-12 16:33:19 -0300

    Adicionada estrutura de envio de mensagens. Interface modificada para conter conteúdo das mensagens. Recebimento de informações em estágio inicial.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,7 +4,6 @@ function closeSideMenu() {
     sideMenu.classList.add("hidden")
 }
 
-
 function showSideMenu() {
     console.log('side-menu')
     const sideMenu = document.querySelector(".side-menu")
@@ -14,4 +13,44 @@ function showSideMenu() {
 function hideIntro() {
     const introMenu = document.querySelector(".intro-screen")
     introMenu.classList.add("hidden")
+}
+
+
+// PROVAVELMENTE DA PRA USAR UM MÉTODO SÓ E ELE PEGA O TIPO DA MSG OU POR SWITCH-CASE
+// OU PELA STRING DA RESPOSTA MSM, ISSO AQUI É TEMPORÁRIO, MAS WAIFUS SÃO ETERNAS
+function writeSystemMsg(response) {
+    const msgBox = document.querySelector(".message-area")
+    // adicionar espaço na resposta do servidor na hora de colocar no template string
+    msgBox.innerHTML += `
+    <div class="system messages">
+        <span class="system">(oiahora)</span>
+        <span class="user-context">oionome</span>
+        <span>entrou na sala</span>
+    </div>`
+}
+
+function writePrivateMsg(response) {
+    const msgBox = document.querySelector(".message-area")
+    // adicionar espaço na resposta do servidor na hora de colocar no template string
+    msgBox.innerHTML += `
+    <div class="private messages">
+        <span class="system">(oiahora)</span>
+        <span class="user-context">oionome</span>
+        <span>para</span>
+        <span class="user-context">oiofulano:</span>
+        <span class="msg-data">oianude</span>
+    </div>`
+}
+
+function writeGlobalMsg(response) {
+    const msgBox = document.querySelector(".message-area")
+    // adicionar espaço na resposta do servidor na hora de colocar no template string
+    msgBox.innerHTML += `
+    <div class="global messages">
+        <span class="system">(oiahora)</span>
+        <span class="user-context">oionome</span>
+        <span>para</span>
+        <span class="user-context">oiofulano:</span>
+        <span class="msg-data">oianude</span>
+    </div>`
 }
