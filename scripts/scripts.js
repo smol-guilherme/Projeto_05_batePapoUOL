52802335bf57a9b8702dc5988b4e09def29986bb
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2022-04-13 10:14:22 -0300

    Tela de login funcional. É possível um usuário se conectar e manter-se conectado através de um handshake.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,8 @@
+const API_URL = "https://mock-api.driven.com.br/api/v6/uol/"
+let loginToken = 0;
+let currentUser = {
+    name: ""
+}
 
 function closeSideMenu() {
     const sideMenu = document.querySelector(".side-menu")
@@ -13,6 +18,11 @@ function showSideMenu() {
 function hideIntro() {
     const introMenu = document.querySelector(".intro-screen")
     introMenu.classList.add("hidden")
+}
+
+function showIntro() {
+    const introMenu = document.querySelector(".intro-screen")
+    introMenu.classList.remove("hidden")
 }
 
 
@@ -53,4 +63,89 @@ function writeGlobalMsg(response) {
         <span class="user-context">oiofulano:</span>
         <span class="msg-data">oiatrova</span>
     </div>`
+}
+
+function registerUser(btn) {
+    const nameReq = {
+        name: btn.parentNode.querySelector("input").value
+    }
+    console.log(nameReq)
+    const promise = axios.post(API_URL+"participants", nameReq)
+    currentUser.name = nameReq.name
+    promise.then(userLogin);
+    promise.catch(errorHandle)
+}
+
+function userLogin() {
+    hideIntro()
+    loginToken = setInterval(serverHandshake, 4000);
+}
+
+function serverHandshake() {
+    const promise = axios.post(API_URL+"status", currentUser)
+    promise.then(console.log('ok'))
+    promise.catch(errorHandle)
+}
+
+function errorHandle(code) {
+    const errorCode = code.response.status;
+    switch (errorCode) {
+        case 400:
+            console.log(errorCode, code.response)
+            alert("Usuário já existe, escolha outro nome.")
+            showIntro()
+            break;
+        case 498:
+            console.log(errorCode, code.response)
+            clearInterval(loginToken)
+            alert("Login expirado, faça um novo login")
+            showIntro()
+            break;
+        case 500:
+            console.log(errorCode, code.response)
+            clearInterval(loginToken)
+            alert("Erro ao acessar o servidor, tente novamente mais tarde.")
+            showIntro()
+            break;
+        case 502:
+            console.log(errorCode, code.response)
+            clearInterval(loginToken)
+            alert("Erro ao acessar o servidor, tente novamente mais tarde.")
+            showIntro()
+            break;
+        case 503:
+            console.log(errorCode, code.response)
+            clearInterval(loginToken)
+            alert("Serviço indisponível, tente novamente mais tarde.")
+            showIntro()
+            break;
+        case 504:
+            console.log(errorCode, code.response)
+            clearInterval(loginToken)
+            alert("Timeout.")
+            showIntro()
+            break;
+        case 521:
+            console.log(errorCode, code.response)
+            clearInterval(loginToken)
+            alert("Erro ao acessar o servidor, tente novamente mais tarde.")
+            showIntro()
+            break;
+        case 523:
+            console.log(errorCode, code.response)
+            clearInterval(loginToken)
+            alert("Erro ao acessar o servidor, tente novamente mais tarde.")
+            showIntro()
+            break;
+        case 599:
+            console.log(errorCode, code.response)
+            clearInterval(loginToken)
+            alert("Erro ao acessar o servidor, tente novamente mais tarde.")
+            showIntro()
+            break;
+        default:
+            alert("Erro desconhecido")
+            showIntro()
+            break;
+    }
 }
