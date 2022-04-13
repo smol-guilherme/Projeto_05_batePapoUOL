1d5efe123af2c68b580fb6b226fd98d4dcf7cb1d
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2022-04-13 14:04:26 -0300

    Implementado: As prim eiras 100 mensagens permanecem na tela, as demais são removidas. Isto é atualizado a cada 3 segundos

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,11 @@
 const API_URL = "https://mock-api.driven.com.br/api/v6/uol/"
 let loginToken = 0;
+let chatReqToken = 0;
+let participantsToken = 0;
 let currentUser = {
     name: ""
 }
+let currentParticipants = [];
 
 function closeSideMenu() {
     const sideMenu = document.querySelector(".side-menu")
@@ -25,20 +28,6 @@ function showIntro() {
     introMenu.classList.remove("hidden")
 }
 
-
-// PROVAVELMENTE DA PRA USAR UM MÉTODO SÓ E ELE PEGA O TIPO DA MSG OU POR SWITCH-CASE
-// OU PELA STRING DA RESPOSTA MSM, ISSO AQUI É TEMPORÁRIO, MAS WAIFUS SÃO ETERNAS
-function writeSystemMsg(response) {
-    const msgBox = document.querySelector(".message-area")
-    // adicionar espaço na resposta do servidor na hora de colocar no template string
-    msgBox.innerHTML += `
-    <div class="system messages">
-        <span class="system">(oiahora)</span>
-        <span class="user-context">oionome</span>
-        <span>entrou na sala</span>
-    </div>`
-}
-
 function writePrivateMsg(response) {
     const msgBox = document.querySelector(".message-area")
     // adicionar espaço na resposta do servidor na hora de colocar no template string
@@ -52,18 +41,32 @@ function writePrivateMsg(response) {
     </div>`
 }
 
-function writeGlobalMsg(response) {
+function writeChatMsg(msgData) {
     const msgBox = document.querySelector(".message-area")
-    // adicionar espaço na resposta do servidor na hora de colocar no template string
-    msgBox.innerHTML += `
-    <div class="global messages">
-        <span class="system">(oiahora)</span>
-        <span class="user-context">oionome</span>
-        <span>para</span>
-        <span class="user-context">oiofulano:</span>
-        <span class="msg-data">oiatrova</span>
-    </div>`
+    const msgCat = msgData.type
+
+    switch (msgCat) {
+        case "message":
+            msgBox.innerHTML += `
+            <div class="global messages">
+                <span class="system">(${msgData.time})&nbsp;</span>
+                <span class="user-context">${msgData.from}&nbsp;</span>
+                <span>para&nbsp;</span>
+                <span class="user-context">${msgData.to}:&nbsp;</span>
+                <span class="msg-data">${msgData.text}</span>
+            </div>`
+            msgBox.firstElementChild.scrollIntoView()
+        case "status":
+            msgBox.innerHTML += `
+            <div class="system messages">
+                <span class="system">(${msgData.time})&nbsp</span>
+                <span class="user-context">${msgData.from}&nbsp</span>
+                <span>${msgData.text}</span>
+            </div>`
+            msgBox.firstElementChild.scrollIntoView()
+    }        
 }
+
 
 function registerUser(btn) {
     const nameReq = {
@@ -76,9 +79,33 @@ function registerUser(btn) {
     promise.catch(errorHandle)
 }
 
+function updateChat() {
+    const promise = axios.get(API_URL+"messages")
+    promise.then(loadChat)
+    promise.catch(errorHandle)
+}
+
+function loadChat(chatLog) {
+    console.log(chatLog)
+    document.querySelector(".message-area").innerHTML = ""
+    const history = chatLog.data.reverse()
+    history.forEach(writeChatMsg)
+}
+
+function usersOnline() {
+    const promise = axios.get(API_URL+"participants")
+    promise.then(updateUsersOnline)
+}
+
+function updateUsersOnline(usersList) {
+    currentParticipants = usersList.data
+}
+
 function userLogin() {
     hideIntro()
+    participantsToken = setInterval(usersOnline, 4000);
     loginToken = setInterval(serverHandshake, 4000);
+    chatReqToken = setInterval(updateChat, 3000);
 }
 
 function serverHandshake() {
@@ -89,6 +116,11 @@ function serverHandshake() {
 
 function errorHandle(code) {
     const errorCode = code.response.status;
+    console.log(errorCode)
+    console.log(code)
+    clearInterval(loginToken)
+    clearInterval(chatReqToken)
+    clearInterval(participantsToken)
     switch (errorCode) {
         case 400:
             console.log(errorCode, code.response)
@@ -97,49 +129,41 @@ function errorHandle(code) {
             break;
         case 498:
             console.log(errorCode, code.response)
-            clearInterval(loginToken)
             alert("Login expirado, faça um novo login")
             showIntro()
             break;
         case 500:
             console.log(errorCode, code.response)
-            clearInterval(loginToken)
             alert("Erro ao acessar o servidor, tente novamente mais tarde.")
             showIntro()
             break;
         case 502:
             console.log(errorCode, code.response)
-            clearInterval(loginToken)
             alert("Erro ao acessar o servidor, tente novamente mais tarde.")
             showIntro()
             break;
         case 503:
             console.log(errorCode, code.response)
-            clearInterval(loginToken)
             alert("Serviço indisponível, tente novamente mais tarde.")
             showIntro()
             break;
         case 504:
             console.log(errorCode, code.response)
-            clearInterval(loginToken)
             alert("Timeout.")
             showIntro()
             break;
         case 521:
             console.log(errorCode, code.response)
-            clearInterval(loginToken)
             alert("Erro ao acessar o servidor, tente novamente mais tarde.")
             showIntro()
             break;
         case 523:
             console.log(errorCode, code.response)
-            clearInterval(loginToken)
             alert("Erro ao acessar o servidor, tente novamente mais tarde.")
             showIntro()
             break;
         case 599:
             console.log(errorCode, code.response)
-            clearInterval(loginToken)
             alert("Erro ao acessar o servidor, tente novamente mais tarde.")
             showIntro()
             break;
