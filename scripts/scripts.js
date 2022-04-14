cd6938c6cc43f26993b4bd8708d7d1d28d58eefb
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2022-04-14 14:17:36 -0300

    Menu lateral com interface funcional. É possível selecionar um único usuário (exceto a si mesmo) ou todos os usuários do canal. É possível selecionar a abrangência da mensagem (Pública ou Restrita). Ao selecionar Todos os Usuários, a abrangência é definida como Pública, se assim for preciso.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,34 +5,8 @@ let participantsToken = 0;
 let currentUser = {
     name: ""
 }
-let pvtUser;
+let targetUser = "Todos";
 let currentParticipants = [];
-
-function closeSideMenu() {
-    const sideMenu = document.querySelector(".side-menu")
-    sideMenu.classList.add("hidden")
-}
-
-function showSideMenu() {
-    console.log('side-menu')
-    const sideMenu = document.querySelector(".side-menu")
-    sideMenu.classList.toggle("hidden")
-}
-
-function hideIntro() {
-    const introMenu = document.querySelector(".intro-screen")
-    introMenu.classList.add("hidden")
-}
-
-function showIntro() {
-    const introMenu = document.querySelector(".intro-screen")
-    introMenu.classList.remove("hidden")
-}
-
-function writePrivateMsg(response) {
-    const msgBox = document.querySelector(".message-area")
-    // adicionar espaço na resposta do servidor na hora de colocar no template string
-}
 
 function writeChatMsg(msgData) {
     const msgBox = document.querySelector(".message-area")
@@ -75,28 +49,11 @@ function writeChatMsg(msgData) {
     }        
 }
 
-function sendMessage(btn) {
-    const msgBody = {
-        from: currentUser.name,
-        to: "Todos",
-        text: btn.parentNode.querySelector("input").value,
-        type: "message"
-    }
-    const promise = axios.post(API_URL+"messages", msgBody)
-    btn.parentNode.querySelector("input").value = ""
-    promise.then(updateChat)
-    promise.catch(errorHandle)
-}
-
-function registerUser(btn) {
-    const nameReq = {
-        name: btn.parentNode.querySelector("input").value
-    }
-    console.log(nameReq)
-    const promise = axios.post(API_URL+"participants", nameReq)
-    currentUser.name = nameReq.name
-    promise.then(userLogin);
-    promise.catch(errorHandle)
+function loadChat(chatLog) {
+    console.log(chatLog)
+    document.querySelector(".message-area").innerHTML = ""
+    const history = chatLog.data.reverse()
+    history.forEach(writeChatMsg)
 }
 
 function updateChat() {
@@ -105,11 +62,75 @@ function updateChat() {
     promise.catch(errorHandle)
 }
 
-function loadChat(chatLog) {
-    console.log(chatLog)
-    document.querySelector(".message-area").innerHTML = ""
-    const history = chatLog.data.reverse()
-    history.forEach(writeChatMsg)
+function sendMessage(btn) {
+    const msgContent = btn.parentNode.querySelector("input").value;
+    if(msgContent) 
+    {
+        const msgBody = {
+            from: currentUser.name,
+            to: targetUser,
+            text: msgContent,
+            type: "message"
+        }
+        const promise = axios.post(API_URL+"messages", msgBody)
+        btn.parentNode.querySelector("input").value = ""
+        promise.then(updateChat)
+        promise.catch(errorHandle)
+    }
+}
+
+function cutBlankSpaces(string) {
+    return string.replace(/\W+/, "").replace(/\W+$/, "").replace(/<.+>/, "")
+}
+
+function toggleVisibility(item) {
+    item.classList.add("hidden")
+}
+
+function checkBox(content) {
+    const checks = content.parentNode.querySelectorAll(".selected")
+    checks.forEach(toggleVisibility)
+    content.querySelector(".selected").classList.remove("hidden")
+    const userSelected = cutBlankSpaces(content.querySelector("span").innerHTML)
+    console.log(userSelected)
+    if(userSelected === "Todos") {
+        const isPublic = document.querySelector(".visibility").firstElementChild.lastElementChild.querySelector(".hidden")
+        if(!isPublic)
+            checkBox(document.querySelector(".visibility").firstElementChild)
+    }
+}
+
+function updateSideMenu(userName) {
+    const userList = document.querySelector(".users")
+    userList.innerHTML += 
+    `
+    <li onclick="checkBox(this)">
+        <span>
+            <ion-icon name="person-circle"></ion-icon>
+            ${userName.name}
+        </span>
+        <ion-icon class="selected hidden" name="checkmark-outline"></ion-icon>
+    </li>
+    `
+}
+
+function removeUser(data) {
+    return (data.name !== currentUser.name)
+}
+
+function updateUsersOnline(usersList) {
+    currentParticipants = usersList.data.filter(removeUser)
+    document.querySelector(".users").innerHTML = 
+    `
+    <li onclick="selectUser(this)">
+        <span>
+            <ion-icon name="person-circle"></ion-icon>
+            Todos
+        </span>
+        <ion-icon class="selected hidden" name="checkmark-outline"></ion-icon>
+    </li>
+    `
+    currentParticipants.forEach(updateSideMenu)
 }
 
 function usersOnline() {
@@ -117,8 +138,24 @@ function usersOnline() {
     promise.then(updateUsersOnline)
 }
 
-function updateUsersOnline(usersList) {
-    currentParticipants = usersList.data
+function registerUser(btn) {
+    const userName = btn.parentNode.querySelector("input").value
+    if(userName) {
+        const nameReq = {
+            name: userName
+        }
+        console.log(nameReq)
+        const promise = axios.post(API_URL+"participants", nameReq)
+        currentUser.name = nameReq.name
+        promise.then(userLogin);
+        promise.catch(errorHandle)    
+    }
+}
+
+function serverHandshake() {
+    const promise = axios.post(API_URL+"status", currentUser)
+    promise.then(console.log('ok'))
+    promise.catch(errorHandle)
 }
 
 function userLogin() {
@@ -128,10 +165,25 @@ function userLogin() {
     chatReqToken = setInterval(updateChat, 3000);
 }
 
-function serverHandshake() {
-    const promise = axios.post(API_URL+"status", currentUser)
-    promise.then(console.log('ok'))
-    promise.catch(errorHandle)
+function closeSideMenu() {
+    const sideMenu = document.querySelector(".side-menu")
+    sideMenu.classList.add("hidden")
+}
+
+function showSideMenu() {
+    console.log('side-menu')
+    const sideMenu = document.querySelector(".side-menu")
+    sideMenu.classList.toggle("hidden")
+}
+
+function hideIntro() {
+    const introMenu = document.querySelector(".intro-screen")
+    introMenu.classList.add("hidden")
+}
+
+function showIntro() {
+    const introMenu = document.querySelector(".intro-screen")
+    introMenu.classList.remove("hidden")
 }
 
 function errorHandle(code) {
@@ -144,52 +196,43 @@ function errorHandle(code) {
     switch (errorCode) {
         case 400:
             console.log(errorCode, code.response)
-            alert("Usuário já existe, escolha outro nome.")
-            showIntro()
+            alert("Aconteceu um erro!\nVoltando ao menu inicial...")
             break;
-        case 498:
-            console.log(errorCode, code.response)
-            alert("Login expirado, faça um novo login")
-            showIntro()
-            break;
-        case 500:
-            console.log(errorCode, code.response)
-            alert("Erro ao acessar o servidor, tente novamente mais tarde.")
-            showIntro()
-            break;
-        case 502:
-            console.log(errorCode, code.response)
-            alert("Erro ao acessar o servidor, tente novamente mais tarde.")
-            showIntro()
-            break;
-        case 503:
-            console.log(errorCode, code.response)
-            alert("Serviço indisponível, tente novamente mais tarde.")
-            showIntro()
-            break;
-        case 504:
-            console.log(errorCode, code.response)
-            alert("Timeout.")
-            showIntro()
-            break;
-        case 521:
-            console.log(errorCode, code.response)
-            alert("Erro ao acessar o servidor, tente novamente mais tarde.")
-            showIntro()
-            break;
-        case 523:
-            console.log(errorCode, code.response)
-            alert("Erro ao acessar o servidor, tente novamente mais tarde.")
-            showIntro()
-            break;
-        case 599:
-            console.log(errorCode, code.response)
-            alert("Erro ao acessar o servidor, tente novamente mais tarde.")
-            showIntro()
-            break;
+        // case 498:
+        //     console.log(errorCode, code.response)
+        //     alert("Login expirado, faça um novo login")
+        //     break;
+        // case 500:
+        //     console.log(errorCode, code.response)
+        //     alert("Erro ao acessar o servidor, tente novamente mais tarde.")
+        //     break;
+        // case 502:
+        //     console.log(errorCode, code.response)
+        //     alert("Erro ao acessar o servidor, tente novamente mais tarde.")
+        //     break;
+        // case 503:
+        //     console.log(errorCode, code.response)
+        //     alert("Serviço indisponível, tente novamente mais tarde.")
+        //     break;
+        // case 504:
+        //     console.log(errorCode, code.response)
+        //     alert("Timeout.")
+        //     break;
+        // case 521:
+        //     console.log(errorCode, code.response)
+        //     alert("Erro ao acessar o servidor, tente novamente mais tarde.")
+        //     break;
+        // case 523:
+        //     console.log(errorCode, code.response)
+        //     alert("Erro ao acessar o servidor, tente novamente mais tarde.")
+        //     break;
+        // case 599:
+        //     console.log(errorCode, code.response)
+        //     alert("Erro ao acessar o servidor, tente novamente mais tarde.")
+        //     break;
         default:
             alert("Erro desconhecido")
-            showIntro()
             break;
     }
+    window.location.reload()
 }
